7aa060b60c416761e444c9c8a89340d446e6b689
webmsgr <zapcontact000@gmail.com>
2022-11-08 12:08:20 -0800

    lol

--- src/server/server.ts
+++ src/server/server.ts
@@ -16,6 +16,7 @@ import {
     TextDocumentSyncKind,
     InitializeResult,
     TextEdit,
+    Hover,
 } from "vscode-languageserver/node";
 import { Block, SCS, Statement, _statements } from "schedule-script";
 import { TextDocument } from "vscode-languageserver-textdocument";
@@ -247,31 +248,19 @@ connection.onCompletion(
         // The pass parameter contains the position of the text document in
         // which code complete got requested. For the example we ignore this
         // info and always provide the same completion items.
-        return [
-            {
-                label: "TypeScript",
-                kind: CompletionItemKind.Text,
-                data: 1,
-            },
-            {
-                label: "JavaScript",
-                kind: CompletionItemKind.Text,
-                data: 2,
-            },
-        ];
-    }
-);
+        return _statements.map((e) => {
+            return {
+                label: e,
+                data: e,
+                type: CompletionItemKind.Text,
+            }
+        })
+    });
 
 // This handler resolves additional information for the item selected in
 // the completion list.
 connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-    if (item.data === 1) {
-        item.detail = "TypeScript details";
-        item.documentation = "TypeScript documentation";
-    } else if (item.data === 2) {
-        item.detail = "JavaScript details";
-        item.documentation = "JavaScript documentation";
-    }
+    item.detail = Hovers.get(item.data);
     return item;
 });
 

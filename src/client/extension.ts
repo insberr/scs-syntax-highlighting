5da564465d607c13610f657f320f6edc252e9fbb
Jonah Matteson <jonahmatteson@gmail.com>
2022-11-08 16:39:48 -0800

    syntax highlighting is not correct at all

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -4,7 +4,7 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import { workspace, ExtensionContext, SemanticTokensLegend, TextDocument, CancellationToken, SemanticTokens, SemanticTokensBuilder, languages } from 'vscode';
 
 import {
 	LanguageClient,
@@ -12,6 +12,108 @@ import {
 	ServerOptions,
 	TransportKind
 } from 'vscode-languageclient/node';
+
+
+const tokenTypes = new Map<string, number>();
+const tokenModifiers = new Map<string, number>();
+
+const legend = (function() {
+	const tokenTypesLegend = [
+		'comment', 'string', 'keyword', 'number', 'regexp', 'operator', 'namespace',
+		'type', 'struct', 'class', 'interface', 'enum', 'typeParameter', 'function',
+		'method', 'decorator', 'macro', 'variable', 'parameter', 'property', 'label'
+	];
+	tokenTypesLegend.forEach((tokenType, index) => tokenTypes.set(tokenType, index));
+
+	const tokenModifiersLegend = [
+		'declaration', 'documentation', 'readonly', 'static', 'abstract', 'deprecated',
+		'modification', 'async'
+	];
+	tokenModifiersLegend.forEach((tokenModifier, index) => tokenModifiers.set(tokenModifier, index));
+
+	return new SemanticTokensLegend(tokenTypesLegend, tokenModifiersLegend);
+})();
+
+interface IParsedToken {
+	line: number;
+	startCharacter: number;
+	length: number;
+	tokenType: string;
+	tokenModifiers: string[];
+}
+
+class DocumentSemanticTokensProvider implements DocumentSemanticTokensProvider {
+	async provideDocumentSemanticTokens(document: TextDocument, token: CancellationToken): Promise<SemanticTokens> {
+		const allTokens = this._parseText(document.getText());
+		const builder = new SemanticTokensBuilder();
+		allTokens.forEach((token) => {
+			builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
+		});
+		return builder.build();
+	}
+
+	private _encodeTokenType(tokenType: string): number {
+		if (tokenTypes.has(tokenType)) {
+			return tokenTypes.get(tokenType)!;
+		} else if (tokenType === 'notInLegend') {
+			return tokenTypes.size + 2;
+		}
+		return 0;
+	}
+
+	private _encodeTokenModifiers(strTokenModifiers: string[]): number {
+		let result = 0;
+		for (let i = 0; i < strTokenModifiers.length; i++) {
+			const tokenModifier = strTokenModifiers[i];
+			if (tokenModifiers.has(tokenModifier)) {
+				result = result | (1 << tokenModifiers.get(tokenModifier)!);
+			} else if (tokenModifier === 'notInLegend') {
+				result = result | (1 << tokenModifiers.size + 2);
+			}
+		}
+		return result;
+	}
+
+	private _parseText(text: string): IParsedToken[] {
+		const r: IParsedToken[] = [];
+		const lines = text.split(/\r\n|\r|\n/);
+		for (let i = 0; i < lines.length; i++) {
+			const line = lines[i];
+			let currentOffset = 0;
+			do {
+				const openOffset = line.indexOf('[', currentOffset);
+				if (openOffset === -1) {
+					break;
+				}
+				const closeOffset = line.indexOf(']', openOffset);
+				if (closeOffset === -1) {
+					break;
+				}
+				const tokenData = this._parseTextToken(line.substring(openOffset + 1, closeOffset));
+				r.push({
+					line: i,
+					startCharacter: openOffset + 1,
+					length: closeOffset - openOffset - 1,
+					tokenType: tokenData.tokenType,
+					tokenModifiers: tokenData.tokenModifiers
+				});
+				currentOffset = closeOffset;
+			} while (true);
+		}
+		return r;
+	}
+
+	private _parseTextToken(text: string): { tokenType: string; tokenModifiers: string[]; } {
+		const parts = text.split('.');
+		return {
+			tokenType: parts[0],
+			tokenModifiers: parts.slice(1)
+		};
+	}
+}
+
+// end pian
+
 
 let client: LanguageClient;
 
@@ -52,10 +154,13 @@ export function activate(context: ExtensionContext) {
 		serverOptions,
 		clientOptions
 	);
+	context.subscriptions.push(languages.registerDocumentSemanticTokensProvider({ language: 'scs' }, new DocumentSemanticTokensProvider(), legend));
 
 	// Start the client. This will also launch the server
 	client.start();
-	console.log("Started Schedule Script Server")
+	console.log("Started Schedule Script Server");
+
+
 }
 
 export function deactivate(): Thenable<void> | undefined {
